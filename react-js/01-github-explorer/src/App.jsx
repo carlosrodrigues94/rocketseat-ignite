de9cf5056870d212870aaf785761aa3952c82b44
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-03-08 21:28:16 -0300

    feat: configure project and structure

---
+++ react-js/01-github-explorer/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./styles/global.scss";
+export function App() {
+  return <h1>Hello React Js</h1>;
+}
---
+++ react-js/01-github-explorer/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+/** Path resolve is used because of the difference between slashs on windows and linux/mac */
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
